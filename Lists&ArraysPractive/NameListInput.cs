97dc3e7b0ed79fe0e55caad7b22bf13e874add67
Carlo Yannuzzi <carlogy@Carlos-MacBook-Pro.local>
2023-05-30 20:22:33 -0400

    Initial commit, with first program.

---
+++ Lists&ArraysPractive/NameListInput.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Lists_ArraysPractice
+{
+	public class NameListInput
+	{
+        public static void InputNames()
+        {
+
+            string input;
+            List<string> NamesList = new List<string>();
+            int total;
+
+
+
+
+            do
+            {
+                Console.Write("Enter names, onec your are done enter a blank space to quit: ");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    NamesList.Add(input);
+                }
+
+            } while (!string.IsNullOrEmpty(input));
+
+
+            total = NamesList.Count();
+
+            if (total <= 2)
+            {
+                for (int name = 0; name < total; name++)
+                {
+                    Console.Write("Total names added: " + total);
+                    Console.WriteLine(NamesList[name]);
+                }
+            }
+            else
+            {
+                Console.Write("You have quit the program. ");
+                Console.WriteLine("Total list of names: " + total);
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
---
+++ Lists&ArraysPractive/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+namespace Lists_ArraysPractice
+{
+
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            NameListInput.InputNames();
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
